6f12568a3e27830ae79908e1b6c87dfbbdfe6331
GLetnikova <g.letnikova@gmail.com>
2023-06-24 17:30:59 +0300

    ItigJava

---
+++ Toy.java
@@ -0,0 +1,24 @@
+public class Toy {
+    private int id;
+    private int weight;
+    private String name;
+
+
+    public Toy(int id, int weight ,String name) {
+        this.id = id;
+        this.weight = weight;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+}
